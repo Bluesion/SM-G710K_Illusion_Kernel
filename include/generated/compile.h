bd342f1f27eaf6093fd38c14b774c072fe87bebe
gpillusion <gpillusion@github.com>
2015-03-28 21:50:48 +0900

    First Commit

---
+++ include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 1 */
+/* SMP PREEMPT */
+#define UTS_MACHINE "arm"
+#define UTS_VERSION "#1 SMP PREEMPT Wed Feb 25 12:25:31 KST 2015"
+#define LINUX_COMPILE_BY "illusion"
+#define LINUX_COMPILE_HOST "illusion"
+#define LINUX_COMPILER "gcc version 4.9.3 20150113 (prerelease) (crosstool-NG 1.20.0 - Linaro GCC 2015.01 - Cortex-A7) "
