170e8f705553a3677bc48dec8d9170e093abbd8e
gpillusion <gpillusion@github.com>
2015-07-05 05:46:35 +0900

    Deleted trashes

--- build_kernel.sh
+++ build_kernel.sh
@@ -4,4 +4,4 @@ export ARCH=arm
 export CROSS_COMPILE=/home/illusion/toolchain/linaro/bin/arm-cortex_a7-linux-gnueabihf-
 
 make msm8226-sec_defconfig VARIANT_DEFCONFIG=msm8926-sec_ms01lte_ktt_defconfig
-make -j4
+make -j3
