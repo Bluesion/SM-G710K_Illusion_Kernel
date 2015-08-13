975a03c5b362996a6257a3f399742a4c375e44a5
gpillusion <gpillusion@github.com>
2015-08-13 19:36:24 +0900

    s2w : Improved s2w

--- drivers/input/touchscreen/sweep2wake.c
+++ drivers/input/touchscreen/sweep2wake.c
@@ -67,8 +67,8 @@ MODULE_LICENSE("GPLv2");
 #define S2W_X_B2		S2W_X_B0-70
 
 /* Left -> Right */
-#define S2W_X_B3		S2W_X_B0+120
-#define S2W_X_B4		DEFAULT_S2W_X_MAX-70
+#define S2W_X_B3		S2W_X_B0+60
+#define S2W_X_B4		DEFAULT_S2W_X_MAX-145
 #define S2W_X_B5		DEFAULT_S2W_X_MAX-S2W_X_B0	
 
 /* Resources */
@@ -498,7 +498,7 @@ static ssize_t s2w_sweep2wake_show(struct device *dev,
 static ssize_t s2w_sweep2wake_dump(struct device *dev,
 		struct device_attribute *attr, const char *buf, size_t count)
 {
-	if (buf[0] >= '0' && buf[0] <= '1' && buf[1] == '\n')
+	if (buf[0] >= '0' && buf[0] <= '2' && buf[1] == '\n')
                 if (s2w_switch != buf[0] - '0')
 		        s2w_switch = buf[0] - '0';
 
