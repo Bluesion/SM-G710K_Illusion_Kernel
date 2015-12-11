c84461cf95cec6867232edd98aa34feb89d80212
gpillusion <gpillusion@github.com>
2015-12-12 02:28:45 +0900

    Fixed some stuffs

--- include/generated/compile.h
+++ include/generated/compile.h
@@ -1,7 +1,7 @@
-/* This file is auto generated, version 2 */
+/* This file is auto generated, version 29 */
 /* SMP PREEMPT */
 #define UTS_MACHINE "arm"
-#define UTS_VERSION "#2 SMP PREEMPT Sun Nov 29 13:27:58 KST 2015"
+#define UTS_VERSION "#29 SMP PREEMPT Sat Dec 12 02:22:28 KST 2015"
 #define LINUX_COMPILE_BY "illusion"
 #define LINUX_COMPILE_HOST "illusion"
 #define LINUX_COMPILER "gcc version 4.9.4 20150629 (prerelease) (crosstool-NG 1.20.0 - Linaro GCC 2015.06 - Cortex-A7) "
