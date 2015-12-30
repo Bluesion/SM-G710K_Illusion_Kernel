4c8782bdc17bd4c7b06ad30fe9b8c087f9fd1807
illusion <gpillusion@github.com>
2015-12-30 23:44:08 +0900

    Fix build error

--- build_kernel.sh
+++ build_kernel.sh
@@ -3,5 +3,5 @@
 export ARCH=arm
 export CROSS_COMPILE=/home/illusion/toolchain/UBER-4.9/bin/arm-eabi-
 
-make illusion_defconfig
+make illusion_defconfig VARIANT_DEFCONFIG=msm8926-sec_ms01lte_ktt_defconfig
 make -j4
