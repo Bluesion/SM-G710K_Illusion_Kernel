2819d7a53380361f39b58a5a3e05e6d555a0e91f
gpillusion <gpillusion@github.com>
2016-01-27 17:35:00 +0900

    Updated defconfig & Removed variant_defconfig

--- build_kernel.sh
+++ build_kernel.sh
@@ -3,5 +3,5 @@
 export ARCH=arm
 export CROSS_COMPILE=/home/illusion/toolchain/bin/arm-eabi-
 
-make illusion_defconfig VARIANT_DEFCONFIG=msm8926-sec_ms01lte_ktt_defconfig
+make illusion_defconfig
 make -j3
