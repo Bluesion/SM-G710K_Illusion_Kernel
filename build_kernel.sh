fd98ec7c7de0fc90958e951bebc52e87ff510adf
gpillusion <dnjscjf098@gmail.com>
2016-08-02 13:52:50 +0900

    Changed toolchain from UBER to Hyper

--- build_kernel.sh
+++ build_kernel.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 export ARCH=arm
-export CROSS_COMPILE=/home/illusion/toolchain/bin/arm-eabi-
+export CROSS_COMPILE=/home/illusion/toolchain/hyper/bin/arm-eabi-
 
 make illusion_defconfig
 make -j3
