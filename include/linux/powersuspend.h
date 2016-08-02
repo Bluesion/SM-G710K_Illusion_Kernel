c4d9a48fa3693ea9c4783354e141d9160cea9182
gpillusion <dnjscjf098@gmail.com>
2016-08-02 14:56:40 +0900

    Fixed build error

--- include/linux/powersuspend.h
+++ include/linux/powersuspend.h
@@ -44,6 +44,6 @@ struct power_suspend {
 
 void register_power_suspend(struct power_suspend *handler);
 void unregister_power_suspend(struct power_suspend *handler);
-
+void set_power_suspend_state_autosleep_hook(int new_state);
 void set_power_suspend_state_panel_hook(int new_state);
 #endif
